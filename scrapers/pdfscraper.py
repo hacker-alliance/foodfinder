6e0bee2a03bb5c7fa0a48455ae22262d5dfaa1b0
zenith110 <thequake@live.com>
2020-03-29 21:56:49 -0400

    Added the scraper insert function to the backend scraper

--- scrapers/pdfscraper.py
+++ scrapers/pdfscraper.py
@@ -8,7 +8,6 @@ def changeData(df, original_word, new_word):
     df["DIS"] = df["DIS"].str.replace(original_word, new_word)
 # Fixes up the excel file
 def fix(pdf_file):
-    pdf_file_clean = pdf_file.replace(".csv", "")
     df = pd.read_csv(pdf_file, sep=",")
 
     df.drop_duplicates(subset=None, inplace=True)
@@ -32,9 +31,8 @@ def pdf_scrape(link):
     link_name_without_pdf = link_name.replace(".pdf", "")
     data = tabula.convert_into(link, link_name_without_pdf + ".csv", output_format="csv", pages='all')
     fix(link_name_without_pdf + ".csv")
-    shutil.move(link_name_without_pdf + ".csv", "../dataset/")
     
 
 if __name__ == "__main__":
-    link = sys.argv[1]
+    link = "https://www1.nyc.gov/assets/hra/downloads/pdf/services/efap/EFAP_ACTIVE.pdf"
     pdf_scrape(link)
